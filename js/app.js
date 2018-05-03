fc5143a0868dc68df99fc60ce3db1fde89fc7acb
Ashleigh <ashleighc207@gmail.com>
2018-05-03 11:10:02 -0400

    Add click listener for 5 cats

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 let clicks = document.querySelectorAll('.h1-container');
 let img = document.querySelectorAll('.img');
-let clickCount = [0, 0];
-
+let cats = document.querySelectorAll('.cat-box');
+let toggleSwitch = document.querySelectorAll('.toggle');
+let clickCount = [0, 0, 0, 0, 0];
 
 
 img[0].addEventListener('click', function(){
@@ -11,6 +12,10 @@ img[0].addEventListener('click', function(){
 	`;
 });
 
+toggleSwitch[0].addEventListener('click', function(){
+	cats[0].classList.toggle('display-none');
+});
+
 img[1].addEventListener('click', function(){
 	clickCount[1]++;
 	clicks[1].innerHTML = `
@@ -18,3 +23,39 @@ img[1].addEventListener('click', function(){
 	`;
 });
 
+toggleSwitch[1].addEventListener('click', function(){
+	cats[1].classList.toggle('display-none');
+});
+
+img[2].addEventListener('click', function(){
+	clickCount[2]++;
+	clicks[2].innerHTML = `
+	<h1 class="heading-one">Clicks: ${clickCount[2]}</h1>
+	`;
+});
+
+toggleSwitch[2].addEventListener('click', function(){
+	cats[2].classList.toggle('display-none');
+});
+
+img[3].addEventListener('click', function(){
+	clickCount[3]++;
+	clicks[3].innerHTML = `
+	<h1 class="heading-one">Clicks: ${clickCount[3]}</h1>
+	`;
+});
+
+toggleSwitch[3].addEventListener('click', function(){
+	cats[3].classList.toggle('display-none');
+});
+
+img[4].addEventListener('click', function(){
+	clickCount[4]++;
+	clicks[4].innerHTML = `
+	<h1 class="heading-one">Clicks: ${clickCount[4]}</h1>
+	`;
+});
+
+toggleSwitch[4].addEventListener('click', function(){
+	cats[4].classList.toggle('display-none');
+});
